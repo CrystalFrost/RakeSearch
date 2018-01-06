d5b53f168e9e65995ab88bbeca02e900a4720c6b
CrystalFrost <mmxsubscribe@yandex.ru>
2018-01-06 12:10:31 +0300

    Доработка генератора workunit-ов
    Добавлены ключи --count=... и --directory= ...

--- RakeWuGenerator/RakeWuGenerator/Generator.h
+++ RakeWuGenerator/RakeWuGenerator/Generator.h
@@ -1,4 +1,4 @@
-﻿// Генератор Диагональных латинских квадратов
+// Генератор Диагональных латинских квадратов
 
 # if !defined Generator_h
 # define Generator_h
@@ -12,74 +12,67 @@ using namespace std;
 class Generator
 {
 public:
-	Generator();							// Конструктор по умолчанию
-	Generator(Generator& source);			// Конструктор копировния
-	void GenerateWorkunits(string start, string result, string checkpoint, string temp, int workunits);	// Выполнение генерации заданий
+  Generator();                  // Конструктор по умолчанию
+  Generator(Generator& source); // Конструктор копировния
+  void GenerateWorkunits(string start, string directory, string checkpoint, string temp, int workunits);  // Выполнение генерации заданий
 
-	Generator& operator = (Generator&  value);									// Оператор копирования
-	friend std::ostream& operator << (std::ostream& os, Generator& value);		// Оператор записи состояние генератора
-	friend std::istream& operator >> (std::istream& is, Generator& value);		// Оператор считывания состояния генератора
+  Generator& operator = (Generator&  value);                  // Оператор копирования
+  friend std::ostream& operator << (std::ostream& os, Generator& value);    // Оператор записи состояние генератора
+  friend std::istream& operator >> (std::istream& is, Generator& value);    // Оператор считывания состояния генератора
 
 private:
-	static const int Rank = Square::Rank;	// Ранг квадрата (для удобства)
-	static const int Free = 1;				// Флаг свободного для использования значения
-	static const int Used = 0;				// Флаг задействования значения в какой-либо клетке диагонали | строки | столбца
-	/*static const int CellsInPath = 18;		// Число обрабатываемых клеток для маленького WU. Для диагоналей 9 ранга + 3 клетка в первой строке - 18 клеток*/
-	static const int CellsInPath = 16;		// Число обрабатываемых клеток. Для диагоналей 9 ранга + 1 клетка в первой строке - 16 клеток
-	/*static const int CellsInPath = 7;		// Число обрабатываемых клеток. Для главной диагонали 8 ранга - 7 клеток*/
-	static const int Yes = 1;				// Флаг "Да"
-	static const int No = 0;				// Флаг "Нет"
+  static const int Rank = Square::Rank;  // Ранг квадрата (для удобства)
+  static const int Free = 1;        // Флаг свободного для использования значения
+  static const int Used = 0;        // Флаг задействования значения в какой-либо клетке диагонали | строки | столбца
+  static const int CellsInPath = 16;    // Число обрабатываемых клеток. Для диагоналей 9 ранга + 1 клетка в первой строке - 16 клеток
+  //static const int CellsInPath = 7;    // Число обрабатываемых клеток. Для главной диагонали 8 ранга - 7 клеток
+  static const int Yes = 1;       // Флаг "Да"
+  static const int No = 0;        // Флаг "Нет"
 
-	void Initialize(string start, string result, string checkpoint, string temp, int workunits);	// Инициализация поиска
-	void Start();							// Функция генерации заданий обходом клеток
-	void Reset();							// Сброс всех значений внутренних структур
+  void Initialize(string start, string directory, string checkpoint, string temp, int workunits);  // Инициализация поиска
+  void Start();              // Функция генерации заданий обходом клеток
+  void Reset();              // Сброс всех значений внутренних структур
 
-	void CopyState(Generator& source);		// Копирование состояния
-	void Read(std::istream& is);			// Считывание состояние генератора из потока
-	void Write(std::ostream& os);			// Запись состояния генератора в поток
+  void CopyState(Generator& source); // Копирование состояния
+  void Read(std::istream& is);       // Считывание состояние генератора из потока
+  void Write(std::ostream& os);      // Запись состояния генератора в поток
 
-	Square newSquare;						// Генерируемый квадрат
+  Square newSquare;          // Генерируемый квадрат
 
-	int path[CellsInPath][2];				// Путь заполнения матрицы квадрата - path[i][0] - строка на шаге i, path[i][1] - столбец
-	int keyRowId;							// Идентификатор строки ключевой клетки - по значению которой расчёт будет останавливаться
-	int keyColumnId;						// Идентификатор столбца ключевой клетки
-	int keyValue;							// Значение ключевой клетки, по достижению которого расчёт будет останавливаться
+  int path[CellsInPath][2];  // Путь заполнения матрицы квадрата - path[i][0] - строка на шаге i, path[i][1] - столбец
+  int keyRowId;              // Идентификатор строки ключевой клетки - по значению которой расчёт будет останавливаться
+  int keyColumnId;           // Идентификатор столбца ключевой клетки
+  int keyValue;              // Значение ключевой клетки, по достижению которого расчёт будет останавливаться
 
-	int primary[Rank];						// Содержимое главной диагонали
-	int secondary[Rank];					// Содержимое побочной диагонали
-	int columns[Rank][Rank];				// Матрица значений, использовавшихся в столбцах - columns[значение][столбец] = 0|1. 0 - значение занято. 1 - свободно.
-	int rows[Rank][Rank];					// Матрица значений, использовавшихся в строках - rows[строка][значение] = 0|1
-	int cellsHistory[Rank][Rank][Rank];		// Куб значений, которые использовались для формирования построенной части квадрата - cellsHistory[строка][столбец][значение]
+  int primary[Rank];         // Содержимое главной диагонали
+  int secondary[Rank];       // Содержимое побочной диагонали
+  int columns[Rank][Rank];   // Матрица значений, использовавшихся в столбцах - columns[значение][столбец] = 0|1. 0 - значение занято. 1 - свободно.
+  int rows[Rank][Rank];          // Матрица значений, использовавшихся в строках - rows[строка][значение] = 0|1
+  int cellsHistory[Rank][Rank][Rank];    // Куб значений, которые использовались для формирования построенной части квадрата - cellsHistory[строка][столбец][значение]
 
-	string startParametersFileName;			// Название файла с параметрами запуска расчёта
-	string resultFileName;					// Название файла с результатами
-	string checkpointFileName;				// Название файла контрольной точки
-	string tempCheckpointFileName;			// Временное название файла новой контрольной точки
+  string startParametersFileName; // Название файла с параметрами запуска расчёта
+  string checkpointFileName;      // Название файла контрольной точки
+  string tempCheckpointFileName;  // Временное название файла новой контрольной точки
+  string workunitsDirectory;      // Название каталога, в который необходимо записать файлы заданий
 
-	int isInitialized;						// Флаг успешной инициализации поиска
-	int rowId;								// Идентификатор строки обратываемой клетки
-	int columnId;							// Идентификатор столбца обрабатываемой клетки
-	int cellId;								// Идентификатор клетки в перечне шагов обхода квадрата
-	
-	int snapshotNumber;						// Номер снимка по которым генерируются задания (от i-1 до i)
-	int workunitKeyRowId;					// Строка ключевой клетки, прописываемая в задании
-	int workunitKeyColumnId;				// Столбец ключевой клетки, приписываемый в задании
+  int isInitialized;        // Флаг успешной инициализации поиска
+  int rowId;                // Идентификатор строки обратываемой клетки
+  int columnId;             // Идентификатор столбца обрабатываемой клетки
+  int cellId;               // Идентификатор клетки в перечне шагов обхода квадрата
 
-	int workunitsCount;						// Число заданий, созданных в рамках этого прогона (не входит в контрольную точку!)
-	int workunitsToGenerate;				// Число заданий, которые необходимо сгенерировать (не входит в контрольную точку!)
+  int snapshotNumber;       // Номер снимка по которым генерируются задания (от i-1 до i)
+  int workunitKeyRowId;     // Строка ключевой клетки, прописываемая в задании
+  int workunitKeyColumnId;  // Столбец ключевой клетки, приписываемый в задании
 
-	void CreateCheckpoint();				// Создание контрольной точки
-	void ProcessWorkunit();					// Формирование очередного задания
-	void ProcessWorkunitR8();				// Формирование очередного задания для квадратов 8-го ранга
-	void ProcessWorkunitR9();				// Формирование очередного задания для квадратов 9-го ранга
+  int workunitsCount;       // Число заданий, созданных в рамках этого прогона (не входит в контрольную точку!)
+  int workunitsToGenerate;  // Число заданий, которые необходимо сгенерировать (не входит в контрольную точку!)
 
-	void GatherStatistics();				// Подсчёт статистических показателей (дублирует механизмы ProcessWorkunit*!)
+  void CreateCheckpoint();  // Создание контрольной точки
+  void ProcessWorkunit();   // Формирование очередного задания
+  void ProcessWorkunitR8(); // Формирование очередного задания для квадратов 8-го ранга
+  void ProcessWorkunitR9(); // Формирование очередного задания для квадратов 9-го ранга
 
-	int diagonalCounter;					// Для статистики. Значение счётчика заданий при переходе на новую диагональ
-	int diagonalValue;						// Для статистики. Значение элементов главной диагонали при переходе на её новое значение
-	int diagonalStartNumber;				// Для статичтики. Номер задания с которого начинается диагональ
-
-	string generatorStateHeader;			// Заголовок, после которого в файле параметров или контрольной точки идёт состояние генератора диагональных квадратов
+  string generatorStateHeader;  // Заголовок, после которого в файле параметров или контрольной точки идёт состояние генератора диагональных квадратов
 };
 
 # endif
--- RakeWuGenerator/RakeWuGenerator/main.cpp
+++ RakeWuGenerator/RakeWuGenerator/main.cpp
@@ -1,22 +1,68 @@
-﻿// Генерация заданий для поиска "граблением"
+// Генерация заданий для поиска "граблением"
 
+# include <stdlib.h>
 # include <iostream>
 # include <string>
 # include "Square.h"
 # include "Generator.h"
 
-int main()
+int main(int argsCount, char* argsValues[])
 {
-	int workunitsCount = 1000;
+        // Set the default values of variables
+        int workunitsCount = 1000;
+        string workunitsDirectory = "";
 
-	string startFileName = "start_parameters.txt";
-	string resultFileName = "result.txt";
-	string checkpointFileName = "checkpoint.txt";
-	string tempCheckpointFileName = "checkpoint_new.txt";
+        // Define the start, checkpoint and temporary checkpoint file name
+        string startFileName = "start_parameters.txt";
+        string checkpointFileName = "checkpoint.txt";
+        string tempCheckpointFileName = "checkpoint_new.txt";
 
-	Generator wuGenerator;
-	wuGenerator.GenerateWorkunits(startFileName, resultFileName, checkpointFileName, tempCheckpointFileName, workunitsCount);
+        // Variables for arguments parsing
+        string argument;                            // Processed argument name
+        string paramName;                           // Parsed parameter name
+        string paramValue;                          // Parsed parameter value
+        string countParamName = "--count";          // Parameter name for workunits count
+        string directoryParamName = "--directory";  // Parameter name for workunits place directory
+        string delimiter = "=";                     // Delimiter for split parameter name and value in argument
+        size_t delimeterPosition = -1;              // Reset the value of deimeter position
 
-	cout << "Press any key to exit ... " << endl;
-	cin.get();
+        // Read the input parameters
+        if (argsCount > 0)
+        {
+            for (int i = 0; i < argsCount; i++)
+            {
+                // Process the i-th agument
+                    // Read the parameter name and value from argument
+                    argument = argsValues[i];
+                    delimeterPosition = argument.find(delimiter, 0);
+
+                    if (delimeterPosition != string::npos)
+                    {
+                        paramName = argument.substr(0, delimeterPosition);
+                        paramValue = argument.substr(delimeterPosition + 1);
+                    }
+
+                    // Process the readed parameter
+                    if (paramName == countParamName)
+                    {
+                        workunitsCount = atoi(paramValue.c_str());
+                    }
+
+                    if (paramName == directoryParamName)
+                    {
+                        workunitsDirectory = paramValue;
+                    }
+            }
+        }
+
+        // Explain parsed parameters
+        cout << "Workunits to generate: " << workunitsCount << endl;
+        cout << "Workunits directory: " << workunitsDirectory << endl;
+
+        Generator wuGenerator;
+        wuGenerator.GenerateWorkunits(startFileName, workunitsDirectory, checkpointFileName, tempCheckpointFileName, workunitsCount);
+
+        cout << "Workunits generation complete!" << endl;
+
+        return 0;
 }

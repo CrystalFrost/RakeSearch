54e5ba5b314dfd8951286012339dd54996fe6185
CrystalFrost <mmxsubscribe@yandex.ru>
2018-09-23 00:23:23 +0300

    Add check list of squares

--- Square/Square/main.cpp
+++ Square/Square/main.cpp
@@ -13,23 +13,29 @@ int main(int argsCount, char* argsValues[])
 	string setFileName = "squares_set.txt";			// Имя файла с набором найденных квадратов
 	string edgesFileName = "squares_edges.txt";		// Имя файла с перечнем рёбер графа из ОДЛК
 	string totalsFileName = "graph_totals.txt";		// Имя файла с характеристиками графа из ОДЛК
+	string checkListFileName = "check_list.txt";	// Имя файла с проверочным списком квадратов
+	int checkListSize = 0;							// Размер проверочного списка квадратов
 
 	fstream startFile;								// Файл с начальным квадратом
 	fstream setFile;								// Файл с набором найденных квадратов
 	fstream edgesFile;								// Файл с перечнем рёбер графа из ОДЛК
 	fstream totalsFile;								// Файл с характеристиками графа из ОДЛК
+	fstream checkListFile;							// Файл с проверочным списком квадратов
 
 	orth_mate_search finder;				// Искатель ОДЛК
 
 	const int FullOrthoDegree = Square::Rank*Square::Rank;	// Степерь ортогональности, соответствующая ортогональным квадратам
 
+	vector<Square> checkList;				// Список квадратов на соответствие с которым проверяются найденные
 	vector<vector<int>> startSquareVector;	// Квадрат, с которого начинается поиск
 	vector<vector<vector<int>>> result;		// Результат поиска
 	vector<Square> squaresSet;				// Множество найденных квадратов
 	vector<Square> iterationSet;			// Множество квадратов, найденных в рамках итерации
 	vector<Square> currentSet;				// Множество квадратов, обрабатываемых в рамках текущей итерации
 	Square newSquare;						// Квадрат, добавляемый в множество
+	Square checkSquare;						// Квадрат, добавляемый в список проверки
 	int isNewSquare = 0;					// Флаг нового квадрата
+	int isTheGraph = 0;						// Флаг нахождения квадрата из The Graph
 	int newSquaresInIteration = 0;			// Число новых квадратов, найденных в рамках очередной итерации
 	int edgesCount = 0;						// Число рёбер в графе из ОДЛК
 
@@ -41,8 +47,11 @@ int main(int argsCount, char* argsValues[])
 	string setFileParamName = "--set";			// Название параметра "Файл с набором найденных квадратов"
 	string edgesFileParamName = "--edges";		// Название параметра "Файл с рёбрами графа"
 	string totalsFileParamName = "--totals";	// Название параметра "Файл с характеристиками графа"
+	string checkListParamName = "--check_list";	// Название параметра "Проверочный список квадратов" (для определения того, что столкнулись с The Graph)
+	string checkListSizeParamName = "--check_list_size";	// Название параметра "Длина проверочного списка"
 	string delimiter = "=";						// Разделитель имени параметра и его значения в аргументе программы
 	size_t delimiterPosition = -1;              // Позиция разделителя имени параметра и его значения в аргументе
+	std::string::size_type size;
 
 	// Чтение входных параметров
 	if (argsCount > 0)
@@ -80,6 +89,16 @@ int main(int argsCount, char* argsValues[])
 				{
 					totalsFileName = paramValue;
 				}
+
+				if (paramName == checkListParamName)
+				{
+					checkListFileName = paramValue;
+				}
+
+				if (paramName == checkListSizeParamName)
+				{
+					checkListSize =std::stoi(paramValue, nullptr);
+				}
 		}
 	}
 
@@ -88,6 +107,19 @@ int main(int argsCount, char* argsValues[])
 	cout << "Squares set file name: " << setFileName << endl;
 	cout << "Graph edges file name: " << edgesFileName << endl;
 	cout << "Totals file name: " << totalsFileName << endl;
+	cout << "Check list file name: " << checkListFileName << endl;
+
+	// Считывание квадратов из списка проверки
+	checkListFile.open(checkListFileName, std::ios_base::in);
+
+	if (checkListFile.is_open())
+	{
+		for (int i = 0; i < checkListSize; i++)
+		{
+			checkListFile >> checkSquare;
+			checkList.push_back(checkSquare);
+		}
+	}
 
 	// Считывание списка ДЛК
 	startFile.open(startFileName, std::ios_base::in);
@@ -132,95 +164,117 @@ int main(int argsCount, char* argsValues[])
 			// Очистка рабочего множества
 			currentSet.clear();
 
-			// Проверка найденных квадратов на уникальность и добавление новых квадратов в основное и рабочее множество
+			// Проверка найденных квадратов на нахождение в проверочном списке
 			for (int i = 0; i < iterationSet.size(); i++)
 			{
-				isNewSquare = 1;
-
-				for (int j = 0; j < squaresSet.size() && isNewSquare; j++)
+				for (int j = 0; j < checkListSize; j++)
 				{
-					if (squaresSet[j] == iterationSet[i])
+					if (iterationSet[i] == checkList[j])
 					{
-						isNewSquare = 0;
+						isTheGraph = 1;
 					}
-				}
-
-				if (isNewSquare)
-				{
-					squaresSet.push_back(iterationSet[i]);
-					currentSet.push_back(iterationSet[i]);
-					newSquaresInIteration++;
 				}
 			}
 
-			cout << "Found " << newSquaresInIteration << " new squares " << endl;
-			cout << "Total squares: " << squaresSet.size() << endl;
-		}
-		while (newSquaresInIteration > 0);
-
-	// Запись найденных квадратов в файл
-	cout << "Save squares set ..." << endl;
-
-	setFile.open(setFileName, std::ios_base::out);
-
-	if (setFile.is_open())
-	{
-		for (int i = 0; i < squaresSet.size(); i++)
-		{
-			setFile << squaresSet[i];
-		}
-	}
-
-	setFile.close();
-
-
-	// Построение графа из найденных ОДЛК
-		// Вывод сообщения
-		cout << "Start to count graph edges..." << endl;
-
-		// Открытие файла для записи рёбер графа
-		edgesFile.open(edgesFileName, std::ios_base::out);
-		edgesFile << "IdA;IdB" << endl;
-
-		// Проверка ортогональности ДЛК
-		for (int i = 0; i < squaresSet.size(); i++)
-		{
-			// Поиск квадратов ортогональных i-му квадрату
-			for (int j = i + 1; j < squaresSet.size(); j++)
+			// Проверка найденных квадратов на уникальность и добавление новых квадратов в основное и рабочее множество
+			if (!isTheGraph)
 			{
-				// Проверка ортогональности i-го и j-го квадрата
-				if (Square::OrthoDegree(squaresSet[i], squaresSet[j]) == FullOrthoDegree)
+				for (int i = 0; i < iterationSet.size(); i++)
 				{
-					edgesCount++;
-					edgesFile << i << ";" << j << endl;
+					isNewSquare = 1;
 
-					if (edgesCount % 1000 == 0)
+					for (int j = 0; j < squaresSet.size() && isNewSquare; j++)
 					{
-						cout << "Reach " << edgesCount << " edges" << endl;
+						if (squaresSet[j] == iterationSet[i])
+						{
+							isNewSquare = 0;
+						}
+					}
+
+					if (isNewSquare)
+					{
+						squaresSet.push_back(iterationSet[i]);
+						currentSet.push_back(iterationSet[i]);
+						newSquaresInIteration++;
+					}
+				}
+
+				cout << "Found " << newSquaresInIteration << " new squares " << endl;
+				cout << "Total squares: " << squaresSet.size() << endl;
+			}
+			else
+			{
+				cout << "Found The Graph! Exit." << endl;
+			}
+		}
+		while (newSquaresInIteration > 0 && !isTheGraph);
+
+	// Обработка найденного графа
+	if (!isTheGraph)
+	{
+		// Запись найденных квадратов в файл
+		cout << "Save squares set ..." << endl;
+
+		setFile.open(setFileName, std::ios_base::out);
+
+		if (setFile.is_open())
+		{
+			for (int i = 0; i < squaresSet.size(); i++)
+			{
+				setFile << squaresSet[i];
+			}
+		}
+
+		setFile.close();
+
+		// Построение графа из найденных ОДЛК
+			// Вывод сообщения
+			cout << "Start to count graph edges..." << endl;
+
+			// Открытие файла для записи рёбер графа
+			edgesFile.open(edgesFileName, std::ios_base::out);
+			edgesFile << "IdA;IdB" << endl;
+
+			// Проверка ортогональности ДЛК
+			for (int i = 0; i < squaresSet.size(); i++)
+			{
+				// Поиск квадратов ортогональных i-му квадрату
+				for (int j = i + 1; j < squaresSet.size(); j++)
+				{
+					// Проверка ортогональности i-го и j-го квадрата
+					if (Square::OrthoDegree(squaresSet[i], squaresSet[j]) == FullOrthoDegree)
+					{
+						edgesCount++;
+						edgesFile << i << ";" << j << endl;
+
+						if (edgesCount % 1000 == 0)
+						{
+							cout << "Reach " << edgesCount << " edges" << endl;
+						}
 					}
 				}
 			}
-		}
 
 		// Закрытие файла, хранящего рёбра графа
 		edgesFile.close();
 
-	// Завершение работы
-		// Вывод итогов на экран
-		cout << "--------------------------------" << endl;
-		cout << "Total squares in graph: " << squaresSet.size() << endl;
-		cout << "Total edges count: " << edgesCount << endl;
+		// Завершение работы
+			// Вывод итогов на экран
+			cout << "--------------------------------" << endl;
+			cout << "Total squares in graph: " << squaresSet.size() << endl;
+			cout << "Total edges count: " << edgesCount << endl;
 
-		// Сохранение итогов в файл
-		totalsFile.open(totalsFileName, std::ios_base::out);
+			// Сохранение итогов в файл
+			totalsFile.open(totalsFileName, std::ios_base::out);
 
-		if (totalsFile.is_open())
-		{
-			totalsFile << "Total squares in graph: " << squaresSet.size() << endl;
-			totalsFile << "Total edges count: " << edgesCount << endl;
-		}
+			if (totalsFile.is_open())
+			{
+				totalsFile << "Total squares in graph: " << squaresSet.size() << endl;
+				totalsFile << "Total edges count: " << edgesCount << endl;
+			}
 
-		totalsFile.close();
+			totalsFile.close();
+	}
 
 	return 0;
 }
